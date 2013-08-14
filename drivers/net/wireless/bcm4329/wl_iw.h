3571d34e8ef2d8b9aa224a394bdb2a160079a84c
TheBootloader <TheBootloader@nomail>
2013-08-14 15:00:21 +0200

    Increase WiFi signal tolerance

--- drivers/net/wireless/bcm4329/wl_iw.h
+++ drivers/net/wireless/bcm4329/wl_iw.h
@@ -71,14 +71,14 @@ struct cntry_locales_custom {
 
 
 #define	WL_IW_RSSI_MINVAL		-200 
-#define	WL_IW_RSSI_NO_SIGNAL	-91 
-#define	WL_IW_RSSI_VERY_LOW		-80	
-#define	WL_IW_RSSI_LOW			-70	
-#define	WL_IW_RSSI_GOOD			-68	
-#define	WL_IW_RSSI_VERY_GOOD	-58	
-#define	WL_IW_RSSI_EXCELLENT	-57	
+#define	WL_IW_RSSI_NO_SIGNAL	-95 
+#define	WL_IW_RSSI_VERY_LOW		-85	
+#define	WL_IW_RSSI_LOW			-78	
+#define	WL_IW_RSSI_GOOD			-71	
+#define	WL_IW_RSSI_VERY_GOOD	-67	
+#define	WL_IW_RSSI_EXCELLENT	-62	
 #define	WL_IW_RSSI_INVALID		0 
-#define MAX_WX_STRING 80
+#define MAX_WX_STRING 87
 #define isprint(c) bcm_isprint(c)
 
 #define WL_IW_SET_ACTIVE_SCAN	(SIOCIWFIRSTPRIV+1)
